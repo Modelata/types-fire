156927060c2853796a25f26156e17e4cd6de4cfb
Gwenaël HENOT <gwenael.henot@moventes.com>
2019-11-14 17:18:31 +0100

    ready to test first version

--- src/interfaces/mf-dao.interface.ts
+++ src/interfaces/mf-dao.interface.ts
@@ -3,23 +3,22 @@ import { MFGetOneOptions } from './mf-get-one-options.interface';
 import { MFGetListOptions } from './mf-get-list-options.interface';
 import { MFFile } from './mf-file.interface';
 import { MFSaveOptions } from './mf-save-options.interface';
-import { DocumentReference, DocumentSnapshot } from '../libs/exports';
-import { Observable } from 'rxjs';
+import { DocumentReference, DocumentSnapshot, AsyncType } from '../libs/exports';
 
-export interface MFDaoInterface<M, AsyncType extends Promise<any> | AsyncType extendsObservable<any>> {
+export interface MFDaoInterface<M> {
   collectionPath: string;
 
   getNewModel(data?: Partial<M>, location?: Partial<MFLocation>): M;
   getReference(location: string | MFLocation): DocumentReference<M>;
-  getById(location: string | MFLocation, options?: MFGetOneOptions): Observable<M> | Promise<M>;
-  getByReference(reference: DocumentReference<M>, options?: MFGetOneOptions): Observable<M>;
-  getByPath(path: string, options?: MFGetOneOptions): Observable<M>;
-  getList(location?: Omit<MFLocation, 'id'>, options?: MFGetListOptions): Observable<M[]>;
-  create(data: Partial<M>, location?: string | MFLocation, options?: MFSaveOptions): Promise<M>; // 'set' OR 'add'
-  update(data: Partial<M>, location?: string | MFLocation, options?: MFSaveOptions): Promise<M>; // 'update'
+  getById(location: string | MFLocation, options?: MFGetOneOptions): AsyncType<M>;
+  getByReference(reference: DocumentReference<M>, options?: MFGetOneOptions): AsyncType<M>;
+  getByPath(path: string, options?: MFGetOneOptions): AsyncType<M>;
+  getList(location?: Omit<MFLocation, 'id'>, options?: MFGetListOptions): AsyncType<M[]>;
+  create(data: Partial<M>, location?: string | MFLocation, options?: MFSaveOptions): Promise<M>;
+  update(data: Partial<M>, location?: string | MFLocation, options?: MFSaveOptions): Promise<M>;
   delete(id: string): Promise<void>;
   getModelFromSnapshot(snapshot: DocumentSnapshot<M>): M;
-  getSnapshotFromId(id: string): Observable<DocumentSnapshot<M>>;
+  getSnapshotFromId(id: string): AsyncType<DocumentSnapshot<M>>;
   beforeSave(model: Partial<M>): Promise<Partial<M>>;
   saveFile(fileObject: MFFile, location: string | MFLocation): MFFile;
 }
