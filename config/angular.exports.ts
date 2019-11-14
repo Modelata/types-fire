dfebf4b20115c2d59af1c6f2a8cf8d608cdaeea3
Gwenaël HENOT <gwenael.henot@moventes.com>
2019-11-14 17:40:12 +0100

    fix build tasks

--- config/angular.exports.ts
+++ config/angular.exports.ts
@@ -1,4 +1,9 @@
-export { DocumentReference, DocumentSnapshot, WhereFilterOp, OrderByDirection } from '@firebase/firestore-types';
+import { DocumentReference as FbDocumentReference, DocumentSnapshot as FbDocumentSnapshot } from '@firebase/firestore-types';
+
+export { WhereFilterOp, OrderByDirection } from '@firebase/firestore-types';
 import { Observable } from 'rxjs';
+
+export interface DocumentReference<M> extends FbDocumentReference { }
+export interface DocumentSnapshot<M> extends FbDocumentSnapshot { }
 
 export interface AsyncType<M> extends Observable<M> { };
