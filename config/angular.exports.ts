2d0f178dd04fc1818315c81c29fe61a902651629
louis amstutz <louis.amstutz@moventes.com>
2019-11-19 10:11:12 +0100

    change angular import

--- config/angular.exports.ts
+++ config/angular.exports.ts
@@ -1,9 +1,21 @@
-import { DocumentReference as FbDocumentReference, DocumentSnapshot as FbDocumentSnapshot } from '@firebase/firestore-types';
-
-export { WhereFilterOp, OrderByDirection } from '@firebase/firestore-types';
+import { firestore } from 'firebase/app';
 import { Observable } from 'rxjs';
+export declare type OrderByDirection = firestore.OrderByDirection;
 
-export interface DocumentReference<M> extends FbDocumentReference { }
-export interface DocumentSnapshot<M> extends FbDocumentSnapshot { }
+export declare type WhereFilterOp = firestore.WhereFilterOp;
 
+export interface DocumentReference<M> extends firestore.DocumentReference { }
+export interface DocumentSnapshotExists<T> extends firestore.DocumentSnapshot {
+    readonly exists: true;
+    data(options?: firestore.SnapshotOptions): T;
+}
+export interface DocumentSnapshotDoesNotExist extends firestore.DocumentSnapshot {
+    readonly exists: false;
+    data(options?: firestore.SnapshotOptions): undefined;
+    get(fieldPath: string | firestore.FieldPath, options?: firestore.SnapshotOptions): undefined;
+}
+export declare type DocumentSnapshot<T> = DocumentSnapshotExists<T> | DocumentSnapshotDoesNotExist;
+export interface QueryDocumentSnapshot<T> extends firestore.QueryDocumentSnapshot {
+    data(options?: firestore.SnapshotOptions): T;
+}
 export interface AsyncType<M> extends Observable<M> { }
