944f2c646bde780e85cff5eb1490229148725e02
louis amstutz <louis.amstutz@gmail.com>
2019-11-20 11:42:53 +0100

    alpha13

--- src/interfaces/mf-get-one-options.interface.ts
+++ src/interfaces/mf-get-one-options.interface.ts
@@ -1,4 +1,5 @@
 export interface IMFGetOneOptions {
+  withSnapshot?: boolean;
   completeOnFirst?: boolean;
   cacheable?: boolean;
 }
--- src/interfaces/mf-offset.interface.ts
+++ src/interfaces/mf-offset.interface.ts
@@ -1,6 +1,8 @@
-export interface IMFOffset {
-  startAt?: string;
-  startAfter?: string;
-  endAt?: string;
-  endBefore?: string;
+import { DocumentSnapshot } from '../specifics/exports';
+
+export interface IMFOffset<M> {
+  startAt?: string | DocumentSnapshot<M>;
+  startAfter?: string | DocumentSnapshot<M>;
+  endAt?: string | DocumentSnapshot<M>;
+  endBefore?: string | DocumentSnapshot<M>;
 }
