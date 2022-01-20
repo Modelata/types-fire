38a686b36c663fd61a1c84a73f3849c1f78ad7e0
Lionel Mullot <lionel.mullot@syd.fr>
2022-01-20 16:36:57 +0100

    :package: Update librairies

--- config/angular.exports.ts
+++ config/angular.exports.ts
@@ -1,24 +1,24 @@
-import { firestore } from 'firebase/app';
+import firebase from 'firebase/compat/app';
 import { Observable } from 'rxjs';
 
-export declare type OrderByDirection = firestore.OrderByDirection;
-export declare type WhereFilterOp = firestore.WhereFilterOp;
-export declare type DocumentReference<M> = firestore.DocumentReference;
-export declare type CollectionReference<M> = firestore.CollectionReference;
+export declare type OrderByDirection = firebase.firestore.OrderByDirection;
+export declare type WhereFilterOp = firebase.firestore.WhereFilterOp;
+export declare type DocumentReference<M> = firebase.firestore.DocumentReference;
+export declare type CollectionReference<M> = firebase.firestore.CollectionReference;
 export declare type DocumentSnapshot<T> = DocumentSnapshotExists<T> | DocumentSnapshotDoesNotExist;
 export declare type AsyncType<M> = Observable<M>;
 
-export interface DocumentSnapshotExists<T> extends firestore.DocumentSnapshot {
-  readonly exists: true;
-  data(options?: firestore.SnapshotOptions): T;
+export interface DocumentSnapshotExists<T> extends firebase.firestore.DocumentSnapshot {
+  exists: boolean;
+  data(options?: firebase.firestore.SnapshotOptions): T;
 }
-export interface DocumentSnapshotDoesNotExist extends firestore.DocumentSnapshot {
-  readonly exists: false;
-  data(options?: firestore.SnapshotOptions): undefined;
-  get(fieldPath: string | firestore.FieldPath, options?: firestore.SnapshotOptions): undefined;
+export interface DocumentSnapshotDoesNotExist extends firebase.firestore.DocumentSnapshot {
+  exists: boolean;
+  data(options?: firebase.firestore.SnapshotOptions): undefined;
+  get(fieldPath: string | firebase.firestore.FieldPath, options?: firebase.firestore.SnapshotOptions): undefined;
 }
-export interface QueryDocumentSnapshot<T> extends firestore.QueryDocumentSnapshot {
-  data(options?: firestore.SnapshotOptions): T;
+export interface QueryDocumentSnapshot<T> extends firebase.firestore.QueryDocumentSnapshot {
+  data(options?: firebase.firestore.SnapshotOptions): T;
 }
 
 
